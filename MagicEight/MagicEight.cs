5ca66d3120bc9e3415b4d24facbeecdeb36f39a1
OmenF1 <owen.lingza@outlook.com>
2022-01-14 10:48:45 +0200

    Initial Commit

---
+++ MagicEight/MagicEight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Reddit;
+
+namespace MagicEight
+{
+    class MagicEight
+    {
+        RedditClient reddit;
+
+        public MagicEight(string id, string secret, string token)
+        {
+            try
+            {
+                reddit = new RedditClient(appId: id, appSecret: secret, refreshToken: token);
+                Console.WriteLine($"Hello I am {reddit.Account.Me.Name} and I  was created on {reddit.Account.Me.Created}.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: \n{e.ToString()}");
+            }
+
+
+
+
+        }
+
+        //public void run()
+        //{
+        //    Boolean Stop = false;
+
+        //    reddit.Account.Messages.MonitorUnread(15000);
+        //    reddit.Account.Messages.UnreadUpdated += C_UnreadMessagesUpdated;
+
+        //    while (!Stop) { }
+
+        //    reddit.Account.Messages.UnreadUpdated -= C_UnreadMessagesUpdated;
+        //    reddit.Account.Messages.MonitorUnread();
+        //}
+
+
+    }
+}
---
+++ MagicEight/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace MagicEight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id = ConfigurationManager.AppSettings.Get("id");
+            string secret = ConfigurationManager.AppSettings.Get("secret");
+            string token = ConfigurationManager.AppSettings.Get("token");
+
+            MagicEight magic = new MagicEight(id, secret, token);
+
+        }
+    }
+}
